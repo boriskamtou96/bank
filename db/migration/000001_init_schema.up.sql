6274d9ec217ed4d270345b52f0525888b81eeb83
Boris Kamtou <boriskamtou@gmail.com>
2026-08-05 01:47:24 +0100

    Added users migration

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,27 +1,37 @@
-CREATE TABLE IF NOT EXISTS "accounts" (
-                                          "id" bigserial PRIMARY KEY,
-                                          "owner" varchar NOT NULL,
-                                          "balance" bigint NOT NULL,
-                                          "currency" varchar NOT NULL,
-                                          "created_at" timestamp NOT NULL DEFAULT (now())
-    );
+CREATE TABLE "users" (
+                         "username" varchar PRIMARY KEY,
+                         "hashed_password" varchar NOT NULL,
+                         "full_name" varchar UNIQUE NOT NULL,
+                         "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
+                         "created_at" timestamptz NOT NULL DEFAULT (now())
+);
 
-CREATE TABLE IF NOT EXISTS "entries" (
-                                         "id" bigserial PRIMARY KEY,
-                                         "account_id" bigint NOT NULL,
-                                         "amount" bigint NOT NULL,
-                                         "created_at" timestamp DEFAULT (now())
-    );
+CREATE TABLE "accounts" (
+                            "id" bigserial PRIMARY KEY,
+                            "owner" varchar NOT NULL,
+                            "balance" bigint NOT NULL,
+                            "currency" varchar NOT NULL,
+                            "created_at" timestamp NOT NULL DEFAULT (now())
+);
 
-CREATE TABLE IF NOT EXISTS "transfers" (
-                                           "id" bigserial PRIMARY KEY,
-                                           "from_account_id" bigint NOT NULL,
-                                           "to_account_id" bigint NOT NULL,
-                                           "amount" bigint NOT NULL,
-                                           "created_at" timestamp DEFAULT (now())
-    );
+CREATE TABLE "entries" (
+                           "id" bigserial PRIMARY KEY,
+                           "account_id" bigint NOT NULL,
+                           "amount" bigint NOT NULL,
+                           "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "transfers" (
+                             "id" bigserial PRIMARY KEY,
+                             "from_account_id" bigint NOT NULL,
+                             "to_account_id" bigint NOT NULL,
+                             "amount" bigint NOT NULL,
+                             "created_at" timestamp DEFAULT (now())
+);
 
 CREATE INDEX ON "accounts" ("owner");
+
+CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
 
 CREATE INDEX ON "entries" ("account_id");
 
@@ -34,6 +44,8 @@ CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 COMMENT ON COLUMN "entries"."amount" IS 'can be negative';
 
 COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
+
+ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username") DEFERRABLE INITIALLY IMMEDIATE;
 
 ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id") DEFERRABLE INITIALLY IMMEDIATE;
 
