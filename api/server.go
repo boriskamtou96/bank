4c0b83240fdf4da6422500d93b0ee342ff8eb729
Boris Kamtou <boriskamtou@gmail.com>
2026-08-05 01:14:52 +0100

    Added custom currency validator

--- api/server.go
+++ api/server.go
@@ -4,24 +4,34 @@ import (
 	db "simplebank/db/sqlc"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
-	store  *db.Store
+	store  db.Store
 	router *gin.Engine
 }
 
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
 	server := &Server{
 		store: store,
 	}
 	router := gin.Default()
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		if err := v.RegisterValidation("currency", validCurrency); err != nil {
+			panic(err)
+		}
+	}
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts/", server.listAccounts)
 	router.PUT("/accounts/", server.updateAccount)
 	router.DELETE("/accounts/:id", server.deleteAccount)
+
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
---
+++ api/validator.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"simplebank/utils"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var validCurrency validator.Func = func(fieldLevel validator.FieldLevel) bool {
+	if currency, ok := fieldLevel.Field().Interface().(string); ok {
+		return utils.IsSupportedCurrency(currency)
+	}
+	return false
+}
